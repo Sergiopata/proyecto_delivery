dd6f7f3f506a65ac78645c4c8e65ba51a3712600
Sergio <sergiospatacino@gmail.com>
2022-02-14 20:53:05 -0300

    Add Functions, etc.

---
+++ script2.js
@@ -0,0 +1,8 @@
+let numeroUsuario = parseInt(prompt('Ingrese el numero'));
+for(let i = 0; i < numeroUsuario; i ++){
+    if (i > 10){
+        break;
+    }
+    console.log ('Este es el ciclo N:' + i)
+
+}
